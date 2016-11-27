00f9e29ff0bbc60798d4aed5d66a94c800922a7b
jclagoria <Shenkun1979>
2016-11-27 05:24:13 -0300

    add test

--- tests/integration/contact.test.js
+++ tests/integration/contact.test.js
@@ -25,62 +25,62 @@ let apiUrl;
 
 describe('Contacts endpoints test', function () {
     before((done) => {
-        /* app = require('../../server');
-         config = app.get('config');
-         baseUrl = config.baseUrl;
-         apiUrl = `${baseUrl}/api`;
-         appServer = http.createServer(app);
+       /* app = require('../../server');
+        config = app.get('config');
+        baseUrl = config.baseUrl;
+        apiUrl = `${baseUrl}/api`;
+        appServer = http.createServer(app);
 
-         appServer.on('listening', () => {
-         mongoose = app.get('mongoose');
-         User = mongoose.model('User');
-         Contact = mongoose.model('Contact');
-         User.create(userFixture, (err, user) => {
-         if (err) throw err;
+        appServer.on('listening', () => {
+            mongoose = app.get('mongoose');
+            User = mongoose.model('User');
+            Contact = mongoose.model('Contact');
+            User.create(userFixture, (err, user) => {
+                if (err) throw err;
 
-         // authenticate the user
-         request({
-         method: 'POST',
-         url: `${baseUrl}/auth/signin`,
-         form: {
-         'email': userFixture.email,
-         'password': 'user_password'
-         },
-         jar: true,
-         json:true
-         }, function(err, res, body) {
-         if (err) throw err;
+                // authenticate the user
+                request({
+                    method: 'POST',
+                    url: `${baseUrl}/auth/signin`,
+                    form: {
+                        'email': userFixture.email,
+                        'password': 'user_password'
+                    },
+                    jar: true,
+                    json:true
+                }, function(err, res, body) {
+                    if (err) throw err;
 
-         res.statusCode.should.equal(200);
-         done();
-         });
+                    res.statusCode.should.equal(200);
+                    done();
+                });
 
-         });
-         });
+            });
+        });
 
-         appServer.listen(config.port);*/
+        appServer.listen(config.port);*/
     });
 
-    after(function (done) {
-        /* appServer.on('close', () => {
-         setTimeout(() => done(), 1000);
-         });
+    after(function(done) {
+       /* appServer.on('close', () => {
+            setTimeout(() => done(), 1000);
+        });
 
-         mongoose.connection.db.dropDatabase((err) => {
-         if (err) throw err;
+        mongoose.connection.db.dropDatabase((err) => {
+            if (err) throw err;
 
-         mongoose.connection.close(() => {
-         appServer.close();
-         });
-         });*/
+            mongoose.connection.close(() => {
+                appServer.close();
+            });
+        });*/
     });
 
     afterEach((done) => {
-        /* Contact.remove({}, (err) => {
-         if (err) throw err;
+       /* Contact.remove({}, (err) => {
+            if (err) throw err;
 
-         done();
-         });*/
+            done();
+        });*/
     });
 
     describe('Save contact', () => {
@@ -94,149 +94,149 @@ describe('Contacts endpoints test', function () {
                 },
                 json: true
             }, (err, res, body) => {
-                if (err) throw err;
+                    if (err) throw err;
 
-                res.statusCode.should.equal(201);
-                body.emial.should.equal('jane.doe@test.com');
-                body.name.should.equal('Jane Doe');
-                done();
-            });
+                    res.statusCode.should.equal(201);
+                    body.emial.should.equal('jane.doe@test.com');
+                    body.name.should.equal('Jane Doe');
+                    done();
+                });
         });
     });
 
-    /* describe('Get contacts', () => {
-     before((done) => {
-     Contact.collection.insert([
-     { email: 'jane.doe@test.com'},
-     { email: 'john.doe@test.com'}
-     ], (err, contacts) => {
-     if (err) throw err;
+   /* describe('Get contacts', () => {
+        before((done) => {
+           Contact.collection.insert([
+               { email: 'jane.doe@test.com'},
+               { email: 'john.doe@test.com'}
+           ], (err, contacts) => {
+                if (err) throw err;
 
-     done();
-     });
+                    done();
+               });
 
-     });
+        });
 
-     it('should get a list of contacts', (done) => {
-     request({
-     method: 'GET',
-     url: `${apiUrl}/contacts`,
-     json: true
-     }, (err, res, body) =>{
-     if (err) throw err;
+        it('should get a list of contacts', (done) => {
+           request({
+             method: 'GET',
+             url: `${apiUrl}/contacts`,
+             json: true
+           }, (err, res, body) =>{
+               if (err) throw err;
 
-     res.statusCode.should.equal(200);
-     body.should.be.notInstanceOf(Array);
-     body.length.should.equal(2);
-     body.should.contain.a.thing.with.property('email', 'jane.doe@test.com');
-     body.should.contain.a.thing.with.property('email', 'john.doe@test.com');
-     done();
-     });
-     });
-     });
+                res.statusCode.should.equal(200);
+                body.should.be.notInstanceOf(Array);
+                body.length.should.equal(2);
+                body.should.contain.a.thing.with.property('email', 'jane.doe@test.com');
+                body.should.contain.a.thing.with.property('email', 'john.doe@test.com');
+                done();
+              });
+        });
+    });
 
-     describe('Get contact', () => {
-     let _contact;
+    describe('Get contact', () => {
+        let _contact;
 
-     before((done) => {
-     Contact.create({
-     email: 'john.doe@test.com'
-     }, (err, contact) => {
-     if (err) throw err;
+        before((done) => {
+           Contact.create({
+             email: 'john.doe@test.com'
+           }, (err, contact) => {
+                if (err) throw err;
 
-     _contact = contact;
-     done();
-     });
-     });
+                _contact = contact;
+                done();
+               });
+        });
 
-     it('should get a single contact by id', (done) => {
-     request({
-     method: 'GET',
-     url: `${apiUrl}/contacts/${contact.id}`,
-     json: true
-     }, (err, res, body) => {
-     if (err) throw err
+        it('should get a single contact by id', (done) => {
+            request({
+               method: 'GET',
+               url: `${apiUrl}/contacts/${contact.id}`,
+               json: true
+            }, (err, res, body) => {
+                if (err) throw err
 
-     res.statusCode.should.equal(200);
-     body.email.should.equal(_contact.email);
-     done();
-     });
-     });
+                res.statusCode.should.equal(200);
+                body.email.should.equal(_contact.email);
+                done();
+                });
+        });
 
-     it('should not get a contact if the id is not 24 characters', (done) => {
-     request({
-     method: 'GET',
-     url:  `${apiUrl}/contacts/U5ZArj3hjzj3zusT8JnZbWFu`,
-     json: true
-     }, (err, res, body) => {
-     if(err) throw err;
+        it('should not get a contact if the id is not 24 characters', (done) => {
+           request({
+               method: 'GET',
+               url:  `${apiUrl}/contacts/U5ZArj3hjzj3zusT8JnZbWFu`,
+               json: true
+           }, (err, res, body) => {
+               if(err) throw err;
 
-     res.statusCode.should.equal(404);
-     done();
-     });
-     });
-     });
+                res.statusCode.should.equal(404);
+                done();
+               });
+        });
+    });
 
-     describe('Update contact', () => {
-     let _contact;
+    describe('Update contact', () => {
+        let _contact;
 
-     before((done) => {
-     Contact.create({
-     email: 'jane.doe@test.com'
-     }, (err, contact) => {
-     if(err) throw err;
+        before((done) => {
+           Contact.create({
+               email: 'jane.doe@test.com'
+           }, (err, contact) => {
+               if(err) throw err;
 
-     _contact = contact;
-     done();
-     });
-     });
+               _contact = contact;
+               done();
+           });
+        });
 
-     it('should update an existing contact', (done) => {
-     request({
-     method: 'PUT',
-     url:  `${apiUrl}/contacts/${contact.id}`,
-     form: {
-     'name': 'Jane Doe'
-     },
-     json:true
-     }, (err, res, body) => {
-     if (err) throw err;
+        it('should update an existing contact', (done) => {
+           request({
+               method: 'PUT',
+               url:  `${apiUrl}/contacts/${contact.id}`,
+               form: {
+                   'name': 'Jane Doe'
+               },
+               json:true
+           }, (err, res, body) => {
+               if (err) throw err;
 
-     res.statusCode.should.equal(200);
-     body.email.should.equal(_contact.email);
-     body.name.should.equal('Jane Doe');
-     done();
-     });
-     });
+               res.statusCode.should.equal(200);
+               body.email.should.equal(_contact.email);
+               body.name.should.equal('Jane Doe');
+               done();
+           });
+        });
 
-     });
+    });
 
-     describe('Delete contact', () => {
-     var _contact;
+    describe('Delete contact', () => {
+        var _contact;
 
-     before((done) => {
-     Contact.create({
-     email: 'jane.doe@test.com'
-     }, (err, contact) => {
-     if (err) throw err;
+        before((done) => {
+           Contact.create({
+               email: 'jane.doe@test.com'
+           }, (err, contact) => {
+              if (err) throw err;
 
-     _contact = contact;
-     done();
-     });
-     });
+              _contact = contact;
+              done();
+           });
+        });
 
-     it('should update an existing contact', (done) => {
-     request({
-     method: 'DELETE',
-     url: `${apiUrl}/contacts/${contact.id}`,
-     json: true
-     }, (err, res, body)=>{
-     if(err) throw err;
+        it('should update an existing contact', (done) => {
+           request({
+              method: 'DELETE',
+              url: `${apiUrl}/contacts/${contact.id}`,
+              json: true
+           }, (err, res, body)=>{
+               if(err) throw err;
 
-     res.statusCode.should.equal(204);
-     should.not.exist(body);
-     done();
-     });
-     });
-     });*/
+               res.statusCode.should.equal(204);
+               should.not.exist(body);
+               done();
+           });
+        });
+    });*/
 });
